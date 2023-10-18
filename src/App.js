745bfd45da6bb2ab1688ec3fb69d3a51d2e3d6fe
Aniketmehraa <annumehra04@gmail.com>
2023-10-18 15:24:45 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark" className="header">
         <Container>
-          <Navbar.Brand href="#home">Firebase CRUD</Navbar.Brand>
+          <Navbar.Brand href="#home">Firebase-CRUD</Navbar.Brand>
         </Container>
       </Navbar>
 
